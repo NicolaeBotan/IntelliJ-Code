e08136c312c8891f1ccf1d9898d1443f3f6c5f78
Nicolae <botan.nicolae94@icloud.com>
2024-06-01 15:57:49 -0600

    add GitPractice

---
+++ src/test/java/GitPractice.java
@@ -0,0 +1,15 @@
+public class GitPractice {
+    public static void main(String[] args) {
+
+        //GIT is a software that is installed on your machine
+        // It is a tool to follow your changes in code
+
+        System.out.println("Git Practice");
+        System.out.println(" git Practice1 command ");
+
+
+
+
+
+    }
+}
