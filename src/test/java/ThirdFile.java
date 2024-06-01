a706da2caa80c0faf9041bc64c3514e59557f91a
Nicolae <botan.nicolae94@icloud.com>
2024-06-01 17:48:12 -0600

    Adding new Changes

--- src/test/java/ThirdFile.java
+++ src/test/java/ThirdFile.java
@@ -1,5 +1,15 @@
 public class ThirdFile {
     public static void main(String[] args) {
         System.out.println("I am Git Practice");
+
+        String str = "Victor";
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'a') {
+                System.out.println(" I got the 'a' ");
+            }
+
+        }
+        System.out.println("There is no 'a' in this word " + str);
     }
 }
