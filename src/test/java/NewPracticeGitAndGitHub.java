6da58ae85ae947a760e8fbe3933a9a220123b595
Nicolae <botan.nicolae94@icloud.com>
2024-06-01 17:39:06 -0600

    Adding new Changes in code

--- src/test/java/NewPracticeGitAndGitHub.java
+++ src/test/java/NewPracticeGitAndGitHub.java
@@ -1,5 +1,14 @@
 public class NewPracticeGitAndGitHub {
     public static void main(String[] args) {
         System.out.println("I am the New Class Of GitHub");
+
+        String word = "Mentoring";
+        String reversedWord = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversedWord += word.charAt(i);
+        }
+        System.out.println(word.toUpperCase());
+        System.out.println(reversedWord.toUpperCase());
     }
 }
